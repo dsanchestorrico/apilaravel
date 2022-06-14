66608a0bd017dce8621749c606319bde637f5cad
PatoCode <e3gan.root@gmail.com>
2022-06-13 22:23:38 -0400

    RECYCLER FINISH

--- app/src/main/java/com/apilaravel/MainActivity.kt
+++ app/src/main/java/com/apilaravel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.apilaravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.apilaravel.databinding.ActivityMainBinding
@@ -9,40 +10,41 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter:CancionAdapter
-    private var cancionesList = mutableListOf<CancionData>()
+    private val cancionesList = mutableListOf<CancionData>()
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        // no defini el root al cambiar el binding LOL!!!!
+        setContentView(binding.root)
         initRecyclerView()
         canciones()
     }
 
     private fun initRecyclerView() {
+        Log.d("TEST","SUFFLE: "+ Arrays.toString(cancionesList.toTypedArray()))
         adapter = CancionAdapter(cancionesList)
         binding.rvCanciones.layoutManager = LinearLayoutManager(this)
         binding.rvCanciones.adapter = adapter
     }
 
     fun canciones(){
-        //En esta parte se crea procesos en segundo plano debajo del proceso principal
         CoroutineScope(Dispatchers.IO).launch {
-            val texto = findViewById<TextView>(R.id.textView01)
             val can: Response<List<CancionData>>
             can = CancionObject.getCancion().create(CancionApi::class.java).obtenerCanciones()
             val objcancion = can.body()!!
-            //la informacion obtenida pase al primer plano
             runOnUiThread{
                 if(can.isSuccessful){
-                    //texto.text = "Artista: " + objcancion[6].artista
-                    cancionesList = objcancion.toMutableList()
+                    val clist = objcancion
+                    cancionesList.clear()
+                    cancionesList.addAll(clist)
                     adapter.notifyDataSetChanged()
                 }
             }
